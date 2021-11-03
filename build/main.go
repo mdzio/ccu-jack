93e4f5139c393788ffce12fdbd9b1ada05d16423
mdzio <info@ccu-historian.de>
2021-11-03 22:23:52 +0100

    virtual devices: support for external XML-RPC clients fixes #64

--- build/main.go
+++ build/main.go
@@ -12,7 +12,7 @@ import (
 const (
 	logLevel   = logging.InfoLevel
 	appName    = "ccu-jack"
-	appVersion = "2.0.47"
+	appVersion = "2.0.48"
 	appPkg     = "github.com/mdzio/ccu-jack"
 	ldFlags    = "-s -w -X main.appVersion=" + appVersion
 	buildDir   = ".."
