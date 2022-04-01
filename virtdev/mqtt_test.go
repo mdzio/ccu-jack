0562875644a6a84e45d7cf3cf99cee9dd1ce7092
mdzio <info@ccu-historian.de>
2022-04-01 23:00:48 +0200

    fixed shelly payload test

--- virtdev/mqtt_test.go
+++ virtdev/mqtt_test.go
@@ -3,18 +3,18 @@ package virtdev
 import "testing"
 
 func TestExtractorTmpl(t *testing.T) {
-	ex, err := newExtractorTmpl(`{{with parseJSON .}}{{if eq .turn "off"}}0{{else}}{{.brightness}}{{end}}{{end}}`)
+	ex, err := newExtractorTmpl(`{{with parseJSON .}}{{if .ison}}{{.brightness}}{{else}}0{{end}}{{end}}`)
 	if err != nil {
 		t.Fatal(err)
 	}
-	v, err := ex.Extract([]byte(`{"brightness":21,"turn":"off"}`))
+	v, err := ex.Extract([]byte(`{"brightness":21,"ison":false}`))
 	if err != nil {
 		t.Fatal(err)
 	}
 	if v != 0.0 {
 		t.Fatal(v)
 	}
-	v, err = ex.Extract([]byte(`{"brightness":42,"turn":"on"}`))
+	v, err = ex.Extract([]byte(`{"brightness":42,"ison":true}`))
 	if err != nil {
 		t.Fatal(err)
 	}
