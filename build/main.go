02bb78d334d6d9fa30097f54dbf1673b1fd0c0d9
mdzio <info@ccu-historian.de>
2022-08-04 17:58:33 +0200

    bump version

--- build/main.go
+++ build/main.go
@@ -12,7 +12,7 @@ import (
 const (
 	logLevel   = logging.InfoLevel
 	appName    = "ccu-jack"
-	appVersion = "2.2.1"
+	appVersion = "2.3.0"
 	appPkg     = "github.com/mdzio/ccu-jack"
 	ldFlags    = "-s -w -X main.appVersion=" + appVersion
 	buildDir   = ".."
