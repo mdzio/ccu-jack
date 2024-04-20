5e6b2e101eae21d4563668c1d550b5229172c01f
mdzio <info@ccu-historian.de>
2024-04-20 16:40:20 +0200

    update copyright year

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ const (
 	appDisplayName = "CCU-Jack"
 	appName        = "ccu-jack"
 	appDescription = "REST/MQTT-Interface for the HomeMatic CCU"
-	appCopyright   = "(C)2019-2023"
+	appCopyright   = "(C)2019-2024"
 	appVendor      = "info@ccu-historian.de"
 
 	// wait time for ReGaHss before signaling an error
